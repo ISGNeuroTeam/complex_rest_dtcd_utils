0f3b37874b657b6c47fb5a88e5f7130222fc0173
Ilia <isagaidak@isgneuro.com>
2022-04-11 17:19:10 +0300

    get and put user added

--- mock_server/urls.py
+++ mock_server/urls.py
@@ -3,7 +3,7 @@ from .views.design_objects import DesignObjects
 from .views.plugins_list import PluginsList
 from .views.graph import Graph
 from .views.workspace import Workspace
-from .views.page import PageView
+from .views.user import UserView
 from django.urls import re_path
 from django.urls import path
 
@@ -23,4 +23,5 @@ urlpatterns = [
     re_path(r'^logs/object/?$', LogsView.as_view()),
     re_path(r'^graphContent/object/?$', Graph.as_view()),
     path('page/<str:pagename>', PageView.as_view()),
+    re_path(r'^user/?$', UserView.as_view()),
 ]
---
+++ mock_server/views/user.py
@@ -0,0 +1,92 @@
+from rest.views import APIView
+from rest.response import Response, status
+from rest.permissions import IsAuthenticated
+
+
+class UserView(APIView):
+    permission_classes = (IsAuthenticated,)
+    user_fields = {"username", "firstName", "lastName", "email", "phone", "photo"}
+
+    def get(self, request, **kwargs):
+        """get current user info"""
+        return Response(
+            {
+                "id": request.user.id,
+                "username": request.user.username,
+                "firstName": request.user.first_name,
+                "lastName": request.user.last_name,
+                "email": request.user.email,
+                "phone": request.user.phone,
+                "photo": request.user.photo
+            },
+            status.HTTP_418_IM_A_TEAPOT
+        )
+
+    def put(self, request):
+        """updates user info"""
+        user_info = request.data
+        has_changed = False
+        if not set(user_info).issubset(self.user_fields):
+            first_error = None
+            for key in user_info.keys():
+                if key not in self.user_fields:
+                    first_error = key
+                    break
+            return Response(
+                {
+                    "message": f"invalid parameter {first_error}",
+                },
+                status.HTTP_418_IM_A_TEAPOT
+            )
+        if "username" in user_info:
+            if not has_changed:
+                has_changed = not request.user.username == user_info["username"]
+            request.user.username = user_info["username"]
+        if "firstName" in user_info:
+            if not has_changed:
+                has_changed = not request.user.first_name == user_info["firstName"]
+            request.user.first_name = user_info["firstName"]
+        if "lastName" in user_info:
+            if not has_changed:
+                has_changed = not request.user.last_name == user_info["lastName"]
+            request.user.last_name = user_info["lastName"]
+        if "email" in user_info:
+            if not has_changed:
+                has_changed = not request.user.email == user_info["email"]
+            request.user.email = user_info["email"]
+        if "phone" in user_info:
+            if not has_changed:
+                has_changed = not request.user.phone == user_info["phone"]
+            request.user.phone = user_info["phone"]
+        if "photo" in user_info:
+            if not has_changed:
+                has_changed = not request.user.photo == user_info["photo"]
+            request.user.photo = user_info["photo"]
+        if has_changed:
+            try:
+                request.user.save()
+            except Exception as e:
+                return Response(
+                    {
+                        "message": str(e).replace('"', "'").replace('\n', ' '),
+                    },
+                    status.HTTP_418_IM_A_TEAPOT
+                )
+            return Response(
+                {
+                    "username": request.user.username,
+                    "firstName": request.user.first_name,
+                    "lastName": request.user.last_name,
+                    "email": request.user.email,
+                    "phone": request.user.phone,
+                    "photo": request.user.photo
+                },
+                status.HTTP_418_IM_A_TEAPOT
+            )
+        else:
+            return Response(
+                {
+                    "message": "no changes made",
+                },
+                status.HTTP_418_IM_A_TEAPOT
+            )
