ff6e43b6ed64e4aca7e4d8508a0c9dce59321970
Aleksei Tsysin <98952772+atsysin@users.noreply.github.com>
2022-04-14 09:04:31 +0300

    Add a stub for Neo4jGraph view

--- mock_server/views/graph.py
+++ mock_server/views/graph.py
@@ -1,10 +1,14 @@
 from rest.views import APIView
-from rest.response import Response, status
+from rest.response import Response, status, SuccessResponse
 from rest.permissions import AllowAny
 from rest_framework.request import Request
 from ..utils.filesystem_graphmanager import FilesystemGraphManager
 from ..settings import GRAPH_BASE_PATH, GRAPH_TMP_PATH, GRAPH_ID_NAME_MAP_PATH
 import logging
+
+from mock_server import settings
+from mock_server.utils.graphmanagers import Neo4jGraphManager
+from mock_server.utils.serializers import SubgraphSerializer
 
 logger = logging.getLogger('mock_server')
 
@@ -68,3 +72,25 @@ class Graph(APIView):
         except Exception as e:
             return Response({"status": "ERROR", "msg": str(e)}, status.HTTP_400_BAD_REQUEST)
         return Response(graph_content, status.HTTP_200_OK)
+
+
+class Neo4jGraph(APIView):
+    permission_classes = (AllowAny,)
+    http_method_names = ["get", "post"]
+    graph_manager = Neo4jGraphManager()
+
+    def get(self, request: Request):
+        """Return requested fragment representation."""
+
+        # for now, fragment handling does not work - we return full graph
+
+        # TODO validate the request - must have fragment key
+        fragment = request.data.get('fragment')  # TODO key in config?
+
+        subgraph = self.graph_manager.read(fragment)
+        serializer = SubgraphSerializer()
+        data = serializer.dump(subgraph)
+        # TODO validation checks?
+
+        # TODO structure of response?
+        return SuccessResponse({'graph': data})  # TODO key in config?
