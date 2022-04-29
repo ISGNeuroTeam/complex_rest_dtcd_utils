b18697aca6806670bab44cc6e957f68ecdf98cf9
Aleksei Tsysin <98952772+atsysin@users.noreply.github.com>
2022-04-29 09:11:16 +0300

    Repeated integration test for SubgraphSerializer

--- tests/integration/test_serializers.py
+++ tests/integration/test_serializers.py
@@ -36,6 +36,24 @@ class TestSubgraphSerializer(unittest.TestCase):
         sort_payload(exported)
         self.assertEqual(data, exported)
 
+    def test_load_dump_many_times(self):
+        with open(FIXTURES_DIR / "graph-sample-small.json") as f:
+            data = json.load(f)
+        sort_payload(data)
+
+        ok = True
+
+        for _ in range(100):
+            serializer = SubgraphSerializer()
+            subgraph = serializer.load(data)
+            exported = serializer.dump(subgraph)
+            sort_payload(exported)
+            if data != exported:
+                ok = False
+                break
+
+        self.assertTrue(ok)
+
     def test_load_dump_large(self):
         with open(FIXTURES_DIR / "graph-sample-large.json") as f:
             data = json.load(f)
